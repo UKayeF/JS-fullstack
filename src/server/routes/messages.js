642675b3630e235fe001b5464ad9b7a3d7e68127
UKayeF <nilseidmann@gmail.com>
2020-06-21 23:19:32 +0200

    expect a token in query string

--- src/server/routes/messages.js
+++ src/server/routes/messages.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-
 const Message = require('../models/message.model');
 
 router.route('/send').post((req, res) => {
@@ -13,6 +12,8 @@ router.route('/send').post((req, res) => {
 })
 
 router.route('/').get((req, res) => {
+  const userToken = req.query.token;
+
   Message.find()
     .then(messages => res.json(messages))
     .catch(err => res.status(400).json(`Error: ${err}`));
