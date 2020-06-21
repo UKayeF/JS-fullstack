ff33585ca3b64901132e708b2b75af8dd4558e4b
UKayeF <nilseidmann@gmail.com>
2020-06-21 20:11:38 +0200

    use theme and styles

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 import Login from './screens/Login';
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#57cae7',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#ff8103',
+      contrastText: '#ffffff',
+    },
+  },
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/'>
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/app/src/screens/Login.jsx
+++ src/app/src/screens/Login.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
-import { Button, FormControl, Grid, Input, InputLabel } from '@material-ui/core';
+import { Button, FormControl, Grid, Input, InputLabel, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  fullwidth: {
+    width: '100%',
+  },
+  root: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const Login = () => {
+  const classes = useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -10,27 +21,34 @@ const Login = () => {
   const handleSubmit = evt => console.log(btoa(`${username}:${password}`));
 
   return (
-    <Grid container>
-      <Grid item xs={12} md={6} lg={3}>
-        <form>
-          <FormControl required>
-            <InputLabel htmlFor='login-name'>Benutzername</InputLabel>
-            <Input
-              id='login-name'
-              aria-described-by='Enter username'
-              onChange={handleUserInputChange}
-            />
-          </FormControl>
-          <FormControl required>
-            <InputLabel htmlFor='login-pass'>Passwort</InputLabel>
-            <Input
-              id='login-pass'
-              aria-described-by='Enter password'
-              onChange={handlePassInputChange}
-            />
-          </FormControl>
-          <Button onClick={handleSubmit}>Einloggen</Button>
-        </form>
+    <Grid className={classes.root}
+      container justify='center' direction='row' alignItems='center' style={{ height: '100vh' }}>
+      <Grid item xs={12} md={4} lg={2} className={classes.root}>
+        <Paper elevation={1}>
+          <form>
+            <Grid item xs={12}>
+              <FormControl required>
+                <InputLabel htmlFor='login-name'>Benutzername</InputLabel>
+                <Input
+                  id='login-name'
+                  onChange={handleUserInputChange}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl required>
+                <InputLabel htmlFor='login-pass'>Passwort</InputLabel>
+                <Input
+                  id='login-pass'
+                  onChange={handlePassInputChange}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant='contained' color='primary' onClick={handleSubmit}>Einloggen</Button>
+            </Grid>
+          </form>
+        </Paper>
       </Grid>
     </Grid>
   );
