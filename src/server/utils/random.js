eb25484d63a8150565ba1719d311fd93f0a41bed
UKayeF <nilseidmann@gmail.com>
2020-06-21 23:29:19 +0200

    join to actually obtain a string

--- src/server/utils/random.js
+++ src/server/utils/random.js
@@ -1,5 +1,5 @@
 const hexString = length => Array(length).fill().map(
-  () => Math.floor(Math.random() * 16).toString(16),
+  () => Math.floor(Math.random() * 16).toString(16).join(''),
 )
 
 module.exports = { hexString };
