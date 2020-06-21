b00a5e781a7963221b480b062a33b9db4d728457
UKayeF <nilseidmann@gmail.com>
2020-06-22 01:09:27 +0200

    filter messages by recipient and sender

--- src/server/routes/messages.js
+++ src/server/routes/messages.js
@@ -1,21 +1,31 @@
 const router = require('express').Router();
 const Message = require('../models/message.model');
+const Token = require('../models/token.model');
+const User = require('../models/user.model');
 
 router.route('/send').post((req, res) => {
   const { from, to, title, body } = req.body;
 
-  const newMessage = new Message({from, to, title, body});
+  const newMessage = new Message({ from, to, title, body });
 
   newMessage.save()
     .then(() => res.json('Message sent!'))
     .catch(err => res.status(400).json(`Error: ${err}`));
 })
 
-router.route('/').get((req, res) => {
+router.route('/').get(async (req, res) => {
   const userToken = req.query.token;
+  const matchingToken = await Token.findOne({ 'token': userToken });
 
-  Message.find()
-    .then(messages => res.json(messages))
+  if (!matchingToken) {
+    res.status(401).json('Invalid token!');
+    return;
+  }
+
+  const { id } = await User.findById(matchingToken.user);
+
+  Message.find({ $or: [{ 'to': id }, { 'from': id }] })
+    .then(messages => res.json(messages.concat(id)))
     .catch(err => res.status(400).json(`Error: ${err}`));
 })
 
