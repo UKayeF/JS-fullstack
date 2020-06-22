23194ad0aed77eecce10f3268d6ba8aa78dd53d9
UKayeF <nilseidmann@gmail.com>
2020-06-22 16:36:39 +0200

    extract appbar for reusability

---
+++ src/app/src/Components/Appbar.jsx
@@ -0,0 +1,42 @@
+import { AppBar, Button, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  fullwidth: {
+    width: '100%',
+  },
+  title: {
+    flexGrow: 1,
+  }
+})
+
+const Appbar = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const logout = () => history.push('/');
+  const goTo = path => () => history.push(path);
+
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton edge='start' className={classes.menuButton}>
+          <Menu/>
+        </IconButton>
+        <Typography variant='h6' className={classes.title}>
+          <Button color='inherit' onClick={goTo('/messages')}>
+            Nachrichten
+          </Button>
+        </Typography>
+        <Button color='inherit' onClick={logout}>Ausloggen</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Appbar;
--- src/app/src/screens/Home.jsx
+++ src/app/src/screens/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { AppBar, Button, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Menu } from '@material-ui/icons';
-import { useHistory } from 'react-router-dom';
+import Appbar from '../Components/Appbar';
 
 const useStyles = makeStyles({
   root: {
@@ -18,27 +17,11 @@ const useStyles = makeStyles({
 
 const Home = () => {
   const classes = useStyles();
-  const history = useHistory();
-  const logout = () => history.push('/');
-  const goTo = path => () => history.push(path);
 
   return (
     <div className={classes.root}>
       <Grid container direction='row' className={classes.fullwidth}>
-        <AppBar position='static'>
-          <Toolbar>
-            <IconButton edge='start' className={classes.menuButton}>
-              <Menu/>
-            </IconButton>
-            <Typography variant='h6' className={classes.title}>
-              <Button color='inherit' onClick={goTo('/nachrichten')}>
-                Nachrichten
-              </Button>
-            </Typography>
-            <Button color='inherit' onClick={logout}>Ausloggen</Button>
-          </Toolbar>
-        </AppBar>
-
+        <Appbar />
       </Grid>
     </div>
   );
