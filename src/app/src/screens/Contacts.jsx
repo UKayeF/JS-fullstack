2acec8c97469639e0eca8be953cf3b5548863fb1
UKayeF <nilseidmann@gmail.com>
2020-06-22 22:47:00 +0200

    style contact list

--- src/app/src/screens/Contacts.jsx
+++ src/app/src/screens/Contacts.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import useContacts from '../hooks/useContacts';
 import Appbar from '../Components/Appbar';
-import { Card, CardHeader, List } from '@material-ui/core';
+import { Card, CardHeader, List, ListItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  floatRight: {
+    margin: '0 0 auto auto',
+  },
+  fullwidth: {
+    width: '100%',
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Contacts = () => {
+  const classes = useStyles();
   const contacts = useContacts()
   return (
     <div>
-      <Appbar />
+      <Appbar/>
       <List>
         {
-          contacts ? contacts.map(contact => (
-            <Card>
-              <CardHeader title={contact} />
-            </Card>
-          )) : null
+          (contacts || []).map((contact, index) => (
+            <ListItem key={index}>
+              <Card className={classes.fullwidth}>
+                <CardHeader title={contact}/>
+              </Card>
+            </ListItem>
+          ))
         }
       </List>
     </div>
