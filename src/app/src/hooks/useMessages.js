538a0d1987e64f742fe58d53adf3da6e15a4d440
UKayeF <nilseidmann@gmail.com>
2020-06-22 16:59:03 +0200

    use signal to abort if necessary

--- src/app/src/hooks/useMessages.js
+++ src/app/src/hooks/useMessages.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from 'react';
 import Fetch from '../utils/Fetch';
 
-export default function useMessages(){
+export default function useMessages() {
   const [messages, setMessages] = useState([])
   useEffect(() => {
     const abortController = new AbortController();
 
     const fetchMessages = async () => {
       const token = localStorage.getItem('auth-token');
-      const messages = await new Fetch(`messages?token=${token}`).fetch();
+      const init = { headers: abortController.signal }
+      const messages = await new Fetch(`messages?token=${token}`, init).fetch();
       setMessages(messages);
     }
 
-    if(messages && !messages.length) fetchMessages();
+    if (messages && !messages.length) fetchMessages();
 
     return () => abortController.abort();
   }, [messages])
